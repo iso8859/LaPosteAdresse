c7b33065ff1f705d6406ae609cd46f8a1db188e2
Remi THOMAS <remi@xtware.com>
2019-07-02 17:16:18 +0200

    Add project files.

---
+++ LaPosteAdresse/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Threading.Tasks;
+
+namespace ovh.api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Rest r = new Rest("/GxhiviTj1kZaVtsdAC7zQlXLpo5/W2dQtAI92CvqjFFVrOGJLmx73vqzLlDn2Vp");
+                var adr = "11 chemin des crieurs";
+                string ueadr = System.Net.WebUtility.UrlEncode(adr);
+                dynamic adresses = r.GetAsync<JObject>("https://api.laposte.fr/controladresse/v1/adresses?q=" + ueadr).GetAwaiter().GetResult();
+                foreach (dynamic item in adresses)
+                {
+                    string code = item.Value.code;
+                    string adresse = item.Value.adresse;
+                    dynamic detail = r.GetAsync<JObject>("https://api.laposte.fr/controladresse/v1/adresses/" + code).GetAwaiter().GetResult();
+                    Console.WriteLine(detail);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ LaPosteAdresse/Rest.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace ovh.api
+{
+    public class Rest
+    {
+        string _okapi_key;
+        public Rest(string okapi_key)
+        {
+            _okapi_key = okapi_key;
+        }
+
+        public Dictionary<string, string> Headers = new Dictionary<string, string>();
+
+        public HttpClient Client()
+        {
+            var result = new HttpClient();
+            foreach (var h in Headers)
+                result.DefaultRequestHeaders.Add(h.Key, h.Value);
+            return result;
+        }
+
+        public async Task<HttpResponseMessage> SendAsync(HttpMethod method, string query, string body = null)
+        {
+            var request = new HttpRequestMessage(method, query);
+            if (body != null)
+                request.Content = new StringContent(body, Encoding.UTF8, "application/json; charset=utf-8");
+            var client = Client();
+            client.DefaultRequestHeaders.Add("X-Okapi-Key", _okapi_key);
+
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+            if (!response.IsSuccessStatusCode && response.StatusCode != System.Net.HttpStatusCode.NotFound)
+            {
+                var error = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                throw new Exception("Request failed:" + error);
+            }
+            return response;
+        }
+
+        public async Task<T> GetAsync<T>(string uri)
+        {
+            var response = await SendAsync(HttpMethod.Get, uri);
+            response.EnsureSuccessStatusCode();
+            string s = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(s);
+        }
+
+        public async Task<T> PostAsync<T>(string uri, object data = null)
+        {
+            var response = await SendAsync(HttpMethod.Post, uri, JsonConvert.SerializeObject(data));
+            response.EnsureSuccessStatusCode();
+            var s = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(s);
+        }
+
+        public async Task<T> PutAsync<T>(string uri, object data = null)
+        {
+            var response = await SendAsync(HttpMethod.Put, uri, JsonConvert.SerializeObject(data));
+            response.EnsureSuccessStatusCode();
+            var s = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(s);
+        }
+    }
+}
